8f1b47846ddb554ddd2780a4f8cd3b8bcf8003cf
Branko Bisacki <brankobisacki@gmail.com>
2019-01-06 15:38:12 +0100

    stocks app created

---
+++ src/app/components/summary/summary.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss']
+})
+export class SummaryComponent  {
+
+  @Input() stock: any;
+
+  constructor() { }
+
+  isNegative() {
+    return (this.stock && this.stock.change < 0);
+  }
+
+  isPositive() {
+    return (this.stock && this.stock.change > 0);
+  }
+}
